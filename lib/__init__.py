994e1dbb9f1a02a75be686620b42c46d859ce38b
Victor Gabriel <victorklitzke2@gmail.com>
2022-06-10 17:50:42 -0400

    Bot Test

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,6 +1,51 @@
 # Functions utils
 # By VictorKlitzke(Sem copia safado)
 
-class CallableModule():
-  def __call__(self):
-     print("Salve")
+from csv import writer
+from gridfs import ClientSession
+from pyautogui import *
+from time import sleep as wait
+
+def Novo():
+  wait(5)
+  click(1616, 230)
+  wait(.7)
+  write('Cliente consumidor')
+  wait(.7)
+  press('enter')
+  write('Vendedor Padrao')
+  wait(.7)
+  press('enter')
+  write('Victor')
+  wait(.7)
+  press('enter')
+  write('A Vista')
+  wait(.7)
+  press('enter')
+  wait('.7')
+  press('enter')
+  wait(.7)
+  press('enter')
+  wait('.7')
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from pyautogui import click, press, doubleClick
-from time import sleep as wait
-
 import lib
 
-lib()
+lib.Novo()
