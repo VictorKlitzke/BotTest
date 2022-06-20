f636db2329e6ac2267685fe760db33850a3772d9
Victor Gabriel <victorklitzke2@gmail.com>
2022-06-20 11:00:20 -0400

    V1

--- lib/__init__.py
+++ lib/__init__.py
@@ -7,15 +7,43 @@ from time import sleep as wait
 
 def Novo():
 
+  wait(3)
   print('Inicio')
+  wait(3)
+  print('Faturamento')
   x, y = locateCenterOnScreen('./imgs/faturamento.png')
   click(x, y)
   wait(3)
-  print('Prevenda')
+  print('Pré Venda')
   x, y = locateCenterOnScreen('./imgs/prevenda.png')
   click(x, y)
   wait(3)
-  print('New')
-  x, y = locateCenterOnScreen('./imgs/new.png')
-  click(x, y)
+  print('Novo')
+  x, y = locateCenterOnScreen('./imgs/novo.png')
+  click(1588, 238)
+  press('clique')
+  wait(3)
+  write('CLIENTE CONSUMIDOR')
+  wait(.7)
+  press('enter')
+  write('VENDEDOR PADRAO')
+  wait(.7)
+  press('enter')
+  write('CLIENTE PADRAO')
+  wait(.7)
+  press('enter')
+  write('A Vista')
+  wait(.7)
+  press('enter')
+  wait(.7)
+  press('enter')
+  write('1122')
+  wait(.7)
+  press('enter')
+  wait(.7)
+  
+ 
+
+
+
   
